1cded409f5da1a36096517726c5d5e20fe482787
Alfonso Nishikawa <alfonso.nishikawa@gmail.com>
2013-11-18 21:57:53 +0100

    Creados datos iniciales para Abonado, TipoFormaPago y TipoServicio

--- fluvigestWebApp/db/seeds.rb
+++ fluvigestWebApp/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+abonados = Abonado.create([
+  {nombre: 'Pepe', apellidos: 'Pérez González', telefono: '981666666', razon_social: 'Ni idea', nif: '12345678Y'},
+  {nombre: 'Juan', apellidos: 'Castiñeiras Castiñeiras', telefono: '667142283', nif: '31415926K'},
+  {nombre: 'Sandra', apellidos: 'Pitesti Vieites', telefono: '666666666', razon_social: 'Mafiosillas S.A.', nif: '00000000N'}
+])
+
+tipo_forma_pagos = TipoFormaPago.create([
+  {nombre: 'Efectivo', descripcion: 'Pago en efectivo en ventanilla'},
+  {nombre: 'Domiciliacion bancaria', descripcion: 'Pago periódico automático mediante domiciliación bancaria'}
+])
+
+tipo_servicios = TipoServicio.create([
+  {nombre: 'Agua doméstica'},
+  {nombre: 'Basura doméstica'},
+  {nombre: 'Alcantarillado doméstico'},
+  {nombre: 'Agua empresarial'},
+  {nombre: 'Basura empresarial'},
+  {nombre: 'Alcantarillado empresrial'},
+  {nombre: 'Agua industrial'},
+  {nombre: 'Basura industral'},
+  {nombre: 'Alcantarillado industrial'},
+])
