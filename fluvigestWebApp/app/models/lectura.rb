c01dd7bd97a21fe15027ff92d60d1fc474af0373
pov01 <a@ubuntu.ubuntu-domain>
2013-11-19 00:42:45 +0100

    añadidas validaciones al modelo lecturas

--- fluvigestWebApp/app/models/lectura.rb
+++ fluvigestWebApp/app/models/lectura.rb
@@ -1,2 +1,7 @@
 class Lectura < ActiveRecord::Base
+validates_presence_of :lectura
+validates_presence_of :incidencia
+validates_presence_of :tincidencias_id
+validates_presence_of :usuarios_id
+validates_presence_of :contadores_id
 end
