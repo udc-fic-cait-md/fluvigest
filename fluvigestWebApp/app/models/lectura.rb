9adf7a54b12582496f15ba50c0dd4a1c731d19b2
Eduardo Alonso <easanles@gmail.com>
2013-11-20 21:49:40 +0100

    Quitada restriccion campo incidencia

--- fluvigestWebApp/app/models/lectura.rb
+++ fluvigestWebApp/app/models/lectura.rb
@@ -5,7 +5,6 @@ belongs_to :usuario
 
 
 validates_presence_of :fecha
-validates_presence_of :incidencia
 validates_presence_of :tincidencias_id
 validates_presence_of :usuarios_id
 validates_presence_of :contadores_id
