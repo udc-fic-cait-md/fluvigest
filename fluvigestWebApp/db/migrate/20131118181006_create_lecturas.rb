6c2c0bdfee5c57dd2d13834507008841c28c9d57
Eduardo Alonso <easanles@gmail.com>
2013-11-20 21:36:12 +0100

    Solucionado problema con las migraciones

--- fluvigestWebApp/db/migrate/20131118181006_create_lecturas.rb
+++ fluvigestWebApp/db/migrate/20131118181006_create_lecturas.rb
@@ -1,13 +1,6 @@
 class CreateLecturas < ActiveRecord::Migration
   def change
     create_table :lecturas do |t|
-      t.decimal  :lectura
-      t.string   :incidencia
-      t.datetime :fecha
-      t.decimal  :lectura_anterior
-      t.integer  :usuarios_id
-      t.integer  :contadores_id
-      t.integer  :tincidencias_id
 
       t.timestamps
     end
