ab2847c450067b49ba92cc714f60d8a3db58e423
pov01 <a@ubuntu.ubuntu-domain>
2013-11-20 17:35:57 +0100

    más validaciones

--- fluvigestWebApp/app/models/lectura.rb
+++ fluvigestWebApp/app/models/lectura.rb
@@ -3,10 +3,19 @@ class Lectura < ActiveRecord::Base
 belongs_to :usuario
 
 
-validates_presence_of :lectura
+
 validates_presence_of :fecha
 validates_presence_of :incidencia
 validates_presence_of :tincidencias_id
 validates_presence_of :usuarios_id
 validates_presence_of :contadores_id
+#validates_format_of :lectura, :with =>/\A\d+\.?\d*\Z/
+validates :lectura, numericality: true
+validates :lectura_anterior, numericality: true
+validates :usuarios_id, numericality: { only_integer: true }
+validates :contadores_id, numericality: {only_integer: true}
+
+
+
+
 end
